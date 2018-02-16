f0cdd5b34507f9e554702f03f3c9fb9ec9c209f5
bala98 <36067891+bala98@users.noreply.github.com>
2018-02-16 23:08:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/mainthread', function (req, res) {
-  res.send('this will be served');
+  res.sendFile(path.join(__dirname, 'ui', 'mainthread.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
